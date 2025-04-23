106c5d5a5cc1e5b9ad80d69c34b78591683540ad
MakeMake23 <andy.ledesma.garcia@gmail.com>
2025-04-23 17:12:11 +0400

    ref(git): stop tracking node_modules/ and .docusaurus/

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/maceo/work/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/maceo/work/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/maceo/work/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/maceo/work/docs/src/css/custom.css"),
-];
